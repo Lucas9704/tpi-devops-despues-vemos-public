5a50b3edabd44dee54a81b9c0cbff7da2115ee24
Leonardo Canteros <leo.sebas.065@gmail.com>
2022-12-23 18:20:53 -0300

    sdfdsds

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const routes = require('./routes')
 const app = express()
 app.set('port', process.env.PORT || 9000)
 const dbOptions = {
-    host: 'localhost',
+    host: process.env.DB_URL,
     port: 3306,
     user: 'root',
     password: 'password',
